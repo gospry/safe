0ca31b6ce5bbcbaceedef6495ac95ef318d44f5e
cayasso <cayasso@gmail.com>
2016-08-14 19:48:08 -0600

    [feature] add debug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 'use strict'
 
 import { send } from 'micro'
+import dbg from 'debug'
+
+const debug = dbg('spry')
 
 export default fn => async (req, res) => {
   try {
     return await fn(req, res)
   } catch (e) {
     if (process.env.NODE_ENV !== 'production' && e.stack) {
-      console.error(e.stack)
+      debug(e.stack)
     }
 
     send(res, e.statusCode || 500, {
